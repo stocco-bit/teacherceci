34ef98cd539dea552f858497c65acc2d7234d5dc
Stocco <joaogabrielstocco@gmail.com>
2026-06-21 18:33:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ const menu = document.querySelector('.menu');
 toggle.addEventListener('click', () => {
   menu.classList.toggle('show');
 });
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
